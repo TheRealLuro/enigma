7fe72e8a166c73ee51904e2f8bfd77bc8b4b1e51
therealluro <jkwiatkowski@student.neumont.edu>
2026-03-04 18:29:36 -0700

    maze size increase, website zoom fix?

--- enigma_server/apis/maze/genmaze.py
+++ enigma_server/apis/maze/genmaze.py
@@ -15,8 +15,8 @@ def return_seed(request: Request, difficulty: str, size: int):
 
     if size <= 1:
         raise HTTPException(status_code=400, detail="Invalid size must be greater than 1")
-    elif size >= 200:
-        raise HTTPException(status_code=400, detail="Way too big, please choose below 200")
+    elif size > 250:
+        raise HTTPException(status_code=400, detail="Way too big, please choose 250 or below")
     
     seed_exists = True
 
