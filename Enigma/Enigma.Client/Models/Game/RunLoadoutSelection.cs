f6bdacb1d2f405031da8083a1a1abef2c668acf2
therealluro <jkwiatkowski@student.neumont.edu>
2026-03-04 18:02:11 -0700

    small fixes

--- Enigma/Enigma.Client/Models/Game/RunLoadoutSelection.cs
+++ Enigma/Enigma.Client/Models/Game/RunLoadoutSelection.cs
@@ -7,4 +7,5 @@ public sealed class RunLoadoutSelection
     public string SlotKind { get; set; } = string.Empty;
     public string Kind { get; set; } = string.Empty;
     public int Quantity { get; set; } = 1;
+    public Dictionary<string, object?> EffectConfig { get; set; } = [];
 }
